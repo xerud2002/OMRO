7154efd7112be6a1ab4f028da5b586d7ec5f09ed
Ciprian R <ciprian.rotopanescu@gmail.com>
2025-10-27 20:20:29 +0000

    modified:   next.config.js 	modified:   package-lock.json 	deleted:    pages/admin/requests/new.tsx 	modified:   tsconfig.json

--- next.config.js
+++ next.config.js
@@ -1,24 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
   images: {
-    // ✅ Replaces deprecated "domains" with "remotePatterns"
     remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-      },
-      {
-        protocol: "https",
-        hostname: "www.tomsoderlund.com",
-      },
-      {
-        protocol: "https",
-        hostname: "firebasestorage.googleapis.com",
-      },
-    ],
-  },
+      { protocol: "http", hostname: "localhost" },
+      { protocol: "https", hostname: "www.tomsoderlund.com" },
+      { protocol: "https", hostname: "firebasestorage.googleapis.com" }
+    ]
+  }
 };
 
 module.exports = nextConfig;
